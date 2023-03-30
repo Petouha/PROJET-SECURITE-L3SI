d1ec345017a2e391a53c81bafe16a314310f2e4e
petouha <rachidgames2.rg@gmail.com>
2023-03-30 18:12:31 +0100

    organizing

--- Activity-1/activity1.sh
+++ Activity-1/activity1.sh
@@ -1,7 +1,16 @@
 #creating big file
 
-./create_file.sh
+./src/create_file.sh
 
 #splitting the files
 
-./splitting_files.sh
+./src/splitting_files.sh
+
+#encrypt/decrypt using the AES 128 ECB
+./src/aes_ecb.sh
+#encrypt/decrypt using the AES 128 CBC
+./src/aes_cbc.sh
+#encrypt/decrypt using the DES3 128 ECB
+./src/3des_ecb.sh
+#encrypt/decrypt using the DES3 128 CBC
+./src/3des_cbc.sh
